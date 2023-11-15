fc333ccb25001cf1f7fc4cd9e299796d49563114
vladislavdanilov1 <148307182+vladislavdanilov1@users.noreply.github.com>
2023-11-15 12:03:06 +0300

    TA-09 Добавление основы для проекта

---
+++ text-analysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace text_analysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ text-analysis/TextAnalyser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text_analysis
+{
+    class TextAnalyser
+    {
+        public string Content;
+
+        public string Load(string uri)
+        {
+            Uri Load = new Uri(uri);
+            return default;
+        }
+
+        private string[] PunctuationMarks = { "!", "?", ":", ",", "." };
+    }
+}
