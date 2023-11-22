2b93070e35ed28ef0486d9bbf3c849c89e93b28b
AndrewRodygin <Andruhka111@mail.ru>
2023-11-22 15:55:47 +0300

    Добавил считывание текста из файла в переменную Content в конструктор и в функцию Load. Load также возвращает текст из файла

--- text-analysis/TextAnalyser.cs
+++ text-analysis/TextAnalyser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace text_analysis
 {
@@ -13,14 +14,15 @@ namespace text_analysis
         public string Load(string uri)
         {
             Uri Load = new Uri(uri);
-            return default;
+            Content = File.ReadAllText(Load.OriginalString);
+            return Content;
         }
 
         private string[] PunctuationMarks = { "!", "?", ":", ",", "." };
 
         public TextAnalyser(string path)
         {
-           
+            Content = File.ReadAllText(path);
         }
 
         public TextAnalyser()
